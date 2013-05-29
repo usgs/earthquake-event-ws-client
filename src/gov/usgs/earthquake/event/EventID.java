2a073a09b25565aadaadde9f5e06968cb0be899d
Kyle Schulz <kcschulz@usgs.gov>
2013-05-29 11:29:55 -0600

    Initial implementation of the EventID class

--- src/gov/usgs/earthquake/event/EventID.java
+++ src/gov/usgs/earthquake/event/EventID.java
@@ -5,4 +5,20 @@ package gov.usgs.earthquake.event;
  */
 public class EventID {
 
+	private network, code;
+	
+	/**
+	 * Construct a new EventID from a network and code.
+	 * 
+	 * @param [String] network
+	 * @param [String] code
+	 */
+	public EventID(String network, String code) {
+		this.network = network;
+		this.code = code;
+	}
+
+	public String toString() {
+		return this.network + this.code;
+	}
 }
