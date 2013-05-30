399e651e8821968e5ad41c3baea9b546cda7c61f
Kyle Schulz <kcschulz@usgs.gov>
2013-05-30 14:49:05 -0600

    Fleshed out the EventQuery tests

--- src/gov/usgs/earthquake/event/EventQueryTest.java
+++ src/gov/usgs/earthquake/event/EventQueryTest.java
@@ -49,14 +49,224 @@ public class EventQueryTest {
 		query.setLongitude(test);
 		Assert.assertEquals(test, query.getLongitude());
 	}
-/*
+
 	@Test
-	public void test() {
+	public void testMinRadius() {
 		BigDecimal test = new BigDecimal(.1);
-		query.set(test);
-		Assert.assertEquals(test, query.get());
+		query.setMinRadius(test);
+		Assert.assertEquals(test, query.getMinRadius());
 	}
-*/
+
+	@Test
+	public void testMaxRadius() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMaxRadius(test);
+		Assert.assertEquals(test, query.getMaxRadius());
+	}
+
+	@Test
+	public void testMinLatitude() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMinLatitude(test);
+		Assert.assertEquals(test, query.getMinLatitude());
+	}
+
+	@Test
+	public void testMaxLatitude() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMaxLatitude(test);
+		Assert.assertEquals(test, query.getMaxLatitude());
+	}
+
+	@Test
+	public void testMinLongitude() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMinLongitude(test);
+		Assert.assertEquals(test, query.getMinLongitude());
+	}
+
+	@Test
+	public void testMaxLongitude() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMaxLongitude(test);
+		Assert.assertEquals(test, query.getMaxLongitude());
+	}
+
+	@Test
+	public void testMinDepth() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMinDepth(test);
+		Assert.assertEquals(test, query.getMinDepth());
+	}
+
+	@Test
+	public void testMaxDepth() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMaxDepth(test);
+		Assert.assertEquals(test, query.getMaxDepth());
+	}
+
+	@Test
+	public void testMinMagnitude() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMinMagnitude(test);
+		Assert.assertEquals(test, query.getMinMagnitude());
+	}
+
+	@Test
+	public void testMaxMagnitude() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMaxMagnitude(test);
+		Assert.assertEquals(test, query.getMaxMagnitude());
+	}
+
+	@Test
+	public void testMinMmi() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMinMmi(test);
+		Assert.assertEquals(test, query.getMinMmi());
+	}
+
+	@Test
+	public void testMaxMmi() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMaxMmi(test);
+		Assert.assertEquals(test, query.getMaxMmi());
+	}
+
+	@Test
+	public void testMinCdi() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMinCdi(test);
+		Assert.assertEquals(test, query.getMinCdi());
+	}
+
+	@Test
+	public void testMaxCdi() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMaxCdi(test);
+		Assert.assertEquals(test, query.getMaxCdi());
+	}
+
+	@Test
+	public void testMinGap() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMinGap(test);
+		Assert.assertEquals(test, query.getMinGap());
+	}
+
+	@Test
+	public void testMaxGap() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMaxGap(test);
+		Assert.assertEquals(test, query.getMaxGap());
+	}
+
+	@Test
+	public void testMinSig() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMinSig(test);
+		Assert.assertEquals(test, query.getMinSig());
+	}
+
+	@Test
+	public void testMaxSig() {
+		BigDecimal test = new BigDecimal(.1);
+		query.setMaxSig(test);
+		Assert.assertEquals(test, query.getMaxSig());
+	}
+
+	@Test
+	public void testLimit() {
+		int test = 1;
+		query.setLimit(test);
+		Assert.assertEquals(test, query.getLimit());
+	}
+
+	@Test
+	public void testOffset() {
+		int test = 1;
+		query.setOffset(test);
+		Assert.assertEquals(test, query.getOffset());
+	}
+
+	@Test
+	public void testMinFelt() {
+		int test = 1;
+		query.setMinFelt(test);
+		Assert.assertEquals(test, query.getMinFelt());
+	}
+
+	@Test
+	public void testMagnitudeType() {
+		String test = "test";
+		query.setMagnitudeType(test);
+		Assert.assertEquals(test, query.getMagnitudeType());
+	}
+
+	@Test
+	public void testEventId() {
+		String test = "test";
+		query.setEventId(test);
+		Assert.assertEquals(test, query.getEventId());
+	}
+
+	@Test
+	public void testEventType() {
+		String test = "test";
+		query.setEventType(test);
+		Assert.assertEquals(test, query.getEventType());
+	}
+
+	@Test
+	public void testCatalog() {
+		String test = "test";
+		query.setCatalog(test);
+		Assert.assertEquals(test, query.getCatalog());
+	}
+
+	@Test
+	public void testContributor() {
+		String test = "test";
+		query.setContributor(test);
+		Assert.assertEquals(test, query.getContributor());
+	}
+
+	@Test
+	public void testProductType() {
+		String test = "test";
+		query.setProductType(test);
+		Assert.assertEquals(test, query.getProductType());
+	}
+
+	@Test
+	public void testIncludeAllOrigins() {
+		boolean test = true;
+		query.setIncludeAllOrigins(test);
+		Assert.assertEquals(test, query.getIncludeAllOrigins());
+	}
+
+	@Test
+	public void testIncludeAllMagnitudes() {
+		boolean test = true;
+		query.setIncludeAllMagnitudes(test);
+		Assert.assertEquals(test, query.getIncludeAllMagnitudes());
+	}
+
+	@Test
+	public void testIncludeArrivals() {
+		boolean test = true;
+		query.setIncludeArrivals(test);
+		Assert.assertEquals(test, query.getIncludeArrivals());
+	}
+
+	@Test
+	public void testKmlAnimated() {
+		boolean test = true;
+		query.setKmlAnimated(test);
+		Assert.assertEquals(test, query.getKmlAnimated());
+	}
+
 	@Test
 	public void testOrderBy() {
 		query.setOrderBy(OrderBy.TIME_ASC);
