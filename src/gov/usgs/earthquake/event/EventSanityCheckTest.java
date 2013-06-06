1730cd3e01d92c2149857b140e3c1bf4309e4b98
Jeremy Fee <jmfee@usgs.gov>
2013-06-06 12:38:54 -0600

    Added EventSanityCheckTest for coverage

--- src/gov/usgs/earthquake/event/EventSanityCheckTest.java
+++ src/gov/usgs/earthquake/event/EventSanityCheckTest.java
@@ -1,5 +1,6 @@
 package gov.usgs.earthquake.event;
 
+import java.math.BigDecimal;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.junit.Assert;
@@ -138,4 +139,72 @@ public class EventSanityCheckTest {
 		System.out.println(errors);
 		Assert.assertNotNull(errors);
 	}
+
+	@Test
+	public void testDifferences() {
+		EventSanityCheck timeCheck = new EventSanityCheck(BigDecimal.ONE, null, null, null);
+		EventSanityCheck locationCheck = new EventSanityCheck(null, BigDecimal.ONE, null, null);
+		EventSanityCheck magnitudeCheck = new EventSanityCheck(null, null, BigDecimal.ONE, null);
+		EventSanityCheck depthCheck = new EventSanityCheck(null, null, null, BigDecimal.ONE);
+
+		// comparison
+		EventComparison timeComparison = new EventComparison(BigDecimal.TEN, null, null, null);
+		EventComparison locationComparison = new EventComparison(null, BigDecimal.TEN, null, null);
+		EventComparison magnitudeComparison = new EventComparison(null, null, BigDecimal.TEN, null);
+		EventComparison depthComparison = new EventComparison(null, null, null, BigDecimal.TEN);
+
+		Assert.assertNotNull("time checks time",
+				timeCheck.isMatch(timeComparison));
+		Assert.assertNull("time doesn't check location",
+				timeCheck.isMatch(locationComparison));
+		Assert.assertNull("time doesn't check magnitude",
+				timeCheck.isMatch(magnitudeComparison));
+		Assert.assertNull("time doesn't check depth",
+				timeCheck.isMatch(depthComparison));
+
+		Assert.assertNull("location doesn't check time",
+				locationCheck.isMatch(timeComparison));
+		Assert.assertNotNull("location checks location",
+				locationCheck.isMatch(locationComparison));
+		Assert.assertNull("location doesn't check magnitude",
+				locationCheck.isMatch(magnitudeComparison));
+		Assert.assertNull("location doesn't check depth",
+				locationCheck.isMatch(depthComparison));
+
+		Assert.assertNull("magnitude doesn't check time",
+				magnitudeCheck.isMatch(timeComparison));
+		Assert.assertNull("magnitude doesn't check location",
+				magnitudeCheck.isMatch(locationComparison));
+		Assert.assertNotNull("magnitude checks magnitude",
+				magnitudeCheck.isMatch(magnitudeComparison));
+		Assert.assertNull("magnitude doesn't check depth",
+				magnitudeCheck.isMatch(depthComparison));
+
+		Assert.assertNull("depth doesn't check time",
+				depthCheck.isMatch(timeComparison));
+		Assert.assertNull("depth doesn't check location",
+				depthCheck.isMatch(locationComparison));
+		Assert.assertNull("depth doesn't check magnitude",
+				depthCheck.isMatch(magnitudeComparison));
+		Assert.assertNotNull("depth checks depth",
+				depthCheck.isMatch(depthComparison));
+
+		Assert.assertEquals("time distance", 10.0,
+				timeCheck.getDistance(timeComparison), 1e-6);
+		Assert.assertEquals("time nodistance", 0.0,
+				timeCheck.getDistance(locationComparison), 1e-6);
+		Assert.assertEquals("location distance", 10.0,
+				locationCheck.getDistance(locationComparison), 1e-6);
+		Assert.assertEquals("location nodistance", 0.0,
+				locationCheck.getDistance(magnitudeComparison), 1e-6);
+		Assert.assertEquals("magnitude distance", 10.0,
+				magnitudeCheck.getDistance(magnitudeComparison), 1e-6);
+		Assert.assertEquals("magnitude nodistance", 0.0,
+				magnitudeCheck.getDistance(depthComparison), 1e-6);
+		Assert.assertEquals("depth distance", 10.0,
+				depthCheck.getDistance(depthComparison), 1e-6);
+		Assert.assertEquals("depth nodistance", 0.0,
+				depthCheck.getDistance(timeComparison), 1e-6);
+	}
+
 }
